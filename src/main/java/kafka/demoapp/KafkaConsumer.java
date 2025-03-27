06a40e9fed1973f84ddcbccccc4835b96fc1bfaf
Yeseo Kim <chinkl850@gmail.com>
2025-03-28 08:47:40 +0900

    Feat: 카프카 컨슈머 컴포넌트 추가

---
+++ src/main/java/kafka/demoapp/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package kafka.demoapp;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+    private static final String TOPIC_NAME = "my-topic";
+
+    @KafkaListener(topics = TOPIC_NAME)
+    public void listenMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
