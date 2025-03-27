f8fd20cf4f9a0177495a73f4b0e296f22a9348b1
Yeseo Kim <chinkl850@gmail.com>
2025-03-28 08:42:28 +0900

    Feat: 카프카 프로듀서 컨트롤러 추가

---
+++ src/main/java/kafka/demoapp/controller/ProducerController.java
@@ -0,0 +1,32 @@
+package kafka.demoapp.controller;
+
+import kafka.demoapp.service.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProducerController {
+    private final KafkaProducerService kafkaProducerService;
+
+    // 단순 퍼블리시
+    @RequestMapping("/publish")
+    public String publish(String message) {
+        kafkaProducerService.send(message);
+        return "published message: " + message;
+    }
+
+    // 퍼블리시 + 콜백
+    @RequestMapping("/publish2")
+    public String publishWithCallback(String message) {
+        kafkaProducerService.sendWithCallback(message);
+        return "published a message with callback: " + message;
+    }
+
+    // JSON으로 퍼블리시 + 콜백
+//    @RequestMapping("/publish3")
+//    public String publishJson(MyMessage myMessage) {
+//        kafkaProducerService.sendJson(myMessage);
+//    }
+}
